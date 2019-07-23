c13710f8db688c66953ec0d7a988e3ad030fbaa5
Kelly Salamack <kellysalamackdesign@gmail.com>
2019-07-23 09:24:05 -0400

    server update for sequelize

--- server.js
+++ server.js
@@ -1,21 +1,23 @@
 //dependencies
 const express = require("express");
-const bodyParser = require("body-parser");
+
+const db = require("./models");
 
 const app = express();
-var PORT = process.env.PORT || 8080;
 
 // Serve static content for the app from the "public" directory in the application directory
 app.use(express.static("public"));
 
 // Parse application body
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 // Set Handlebars
 var exphbs = require("express-handlebars");
 
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.engine("handlebars", exphbs({
+    defaultLayout: "main"
+}));
 app.set("view engine", "handlebars");
 
 // Import routes and give the server access to them
@@ -24,8 +26,12 @@ var routes = require("./controllers/burgers_controllers.js");
 app.use(routes);
 
 // Start our server so that it can begin listening to client requests.
-app.listen(PORT, function () {
-    // Log (server-side) when our server has started
-    console.log("Server listening on: http://localhost:" + PORT);
+var PORT = process.env.PORT || 8080;
+db.sequelize.sync().then(function () {
+    app.listen(PORT, function () {
+        // Log (server-side) when our server has started
+        console.log("Server listening on: http://localhost:" + PORT);
+    });
 });
+
 
